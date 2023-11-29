6fcaa6d7357ef4bf9ea3c3533eb61d553f891471
Amerson <amersonsilva3@gmail.com>
2023-11-29 13:44:55 -0300

    Alterações na página de links

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-function toogleMode() {
+function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
+  
   //if (html.classList.contains("light")) {
   //html.classList.remove("light")
   //} else {
